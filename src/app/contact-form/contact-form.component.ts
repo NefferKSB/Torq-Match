640e91005b8b9d52a16d8fed3fa780c3d34aefe9
NefferKSB <kennanrsb@gmail.com>
2023-06-10 23:39:49 -0400

    contact form updates

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,32 +1,25 @@
 import { Component, HostListener, OnInit } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
-import {MatButtonModule} from '@angular/material/button';
-import {MatSelectModule} from '@angular/material/select';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { ResponsiveService } from '../services/responsive-service';
-//import { HttpClient } from "@angular/common/http";
-//import { environment } from 'src/environments/environment';
 import { MailService } from '../services/mail-service';
 
 //const BACKEND_URL = environment.apiUrl;
-
 @Component({
   selector: 'contact-form',
   templateUrl: './contact-form.component.html',
   styleUrls: ['./contact-form.component.css'],
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, MatSelectModule, ReactiveFormsModule, MatButtonModule],
+  imports: [MatFormFieldModule, MatInputModule, MatSelectModule, ReactiveFormsModule, MatButtonModule]
 })
 export class ContactComponent implements OnInit {
   contactForm = this.formBuilder.group(
     {
       contactName: ['', Validators.required],
-      email: [
-        '',
-        Validators.required
-        //Validators.compose([Validators.required, Validators.email]),
-      ],
+      email: ['', Validators.required],
       nameplate: ['', Validators.required],
       motorInfo: ['', Validators.required],
       assembly: ['', Validators.required],
@@ -48,8 +41,6 @@ export class ContactComponent implements OnInit {
   }
 
   constructor(
-    private fb: FormBuilder,
-    //private http: HttpClient,
     private responsiveService: ResponsiveService,
     private formBuilder: FormBuilder,
     public mailService: MailService
@@ -86,8 +77,15 @@ export class ContactComponent implements OnInit {
       alert("not a valid form submission!");
       return;
     }
-    alert("valid form submission!");
-    //this.mailService.sendMail(this.contactForm.value.contactName, this.contactForm.value.email, this.contactForm.value.nameplate, this.contactForm.value.motorInfo, this.contactForm.value.assembly, this.contactForm.value.application, this.contactForm.value.additionalInfo);
+    alert("valid form submission! " + this.contactForm.value.assembly);
+    this.mailService.sendMail(
+      this.contactForm.value.contactName ?? "",
+      this.contactForm.value.email ?? "",
+      this.contactForm.value.nameplate ?? "",
+      this.contactForm.value.motorInfo ?? "",
+      this.contactForm.value.assembly ?? "",
+      this.contactForm.value.application ?? "",
+      this.contactForm.value.additionalInfo ?? "");
     this.contactForm.reset();
   }
 }
