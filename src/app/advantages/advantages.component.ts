8ed86f951f4d47ff075121e3c2b3f6a9daf53c05
NefferKSB <kennanrsb@gmail.com>
2023-06-18 18:50:31 -0400

    responsive updates

--- src/app/advantages/advantages.component.ts
+++ src/app/advantages/advantages.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AnimationOptions } from 'ngx-lottie';
 import { AnimationItem } from 'lottie-web';
+import { ResponsiveService } from '../services/responsive-service';
 
 @Component({
   selector: 'app-advantages',
@@ -13,12 +14,35 @@ export class AdvantagesComponent implements OnInit {
     path: '/assets/lottie/mechanics.json'
   };
 
-  constructor() { }
+  rightPosition: string;
+  screenSize: string = this.responsiveService.screenWidth;
+
+  constructor(private responsiveService: ResponsiveService) {
+    this.rightPosition = "60px";
+  }
 
   ngOnInit(): void {}
 
   // This is the component function that binds to the animationCreated event from the package
   onAnimate(animationItem: AnimationItem): void {
     console.log(animationItem);
+  }
+
+  onResize(event: any){
+    this.responsiveService.checkWidth();
+    this.screenSize = this.responsiveService.screenWidth;
+    this.setResponsiveAttrs(this.screenSize);
+  }
+
+  setResponsiveAttrs(screenSize: string) {
+    if(screenSize === 'lg') {
+      this.rightPosition = "60px";
+    }
+    if(screenSize === 'md') {
+      this.rightPosition = "0px";
+    }
+    if(screenSize === 'sm') {
+      this.rightPosition = "0px";
+    }
   }
 }
